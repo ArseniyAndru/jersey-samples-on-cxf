5a62c2455f57debda1c69a8fc943837c7686d425
Glen Mazza <gmazza@talend.com>
2012-02-24 20:31:19 -0500

    Added CXF version of https-clientserver-grizzly

---
+++ https-clientserver-grizzly/src/main/java/com/sun/jersey/samples/https_grizzly/auth/SecurityFilter.java
@@ -0,0 +1,124 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sun.jersey.samples.https_grizzly.auth;
+
+import org.apache.commons.codec.binary.Base64;
+import java.security.Principal;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.HttpHeaders;
+import java.io.IOException;
+
+public class SecurityFilter implements Filter {
+
+	@Context
+    UriInfo uriInfo;
+    private static final String REALM = "HTTPS Example authentization";
+
+    public void doFilter(ServletRequest req, ServletResponse res,
+            FilterChain chain) throws IOException, ServletException {
+
+    	User user = authenticate((HttpServletRequest) req);
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterConfig config) throws ServletException {    	
+    }
+
+    public void destroy() {
+    }
+    
+    private User authenticate(HttpServletRequest request) {
+        // Extract authentication credentials
+        String authentication = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authentication == null) {
+            throw new AuthenticationException("Authentication credentials are required", REALM);
+        }
+        if (!authentication.startsWith("Basic ")) {
+            return null;
+            // additional checks should be done here
+            // "Only HTTP Basic authentication is supported"
+        }
+        authentication = authentication.substring("Basic ".length());
+        String[] values = new String(Base64.decodeBase64(authentication)).split(":");
+        if (values.length < 2) {
+            throw new WebApplicationException(400);
+            // "Invalid syntax for username and password"
+        }
+        String username = values[0];
+        String password = values[1];
+        if ((username == null) || (password == null)) {
+            throw new WebApplicationException(400);
+            // "Missing username or password"
+        }
+
+        // Validate the extracted credentials
+        User user = null;
+
+        if (username.equals("user") && password.equals("password")) {
+            user = new User("user", "user");
+            System.out.println("USER AUTHENTICATED");
+        //        } else if (username.equals("admin") && password.equals("adminadmin")) {
+        //            user = new User("admin", "admin");
+        //            System.out.println("ADMIN AUTHENTICATED");
+        } else {
+            System.out.println("USER NOT AUTHENTICATED");
+            throw new RuntimeException(new AuthenticationException("Invalid username or password\r\n", REALM));
+        }
+        return user;
+    }
+
+    public class Authorizer implements SecurityContext {
+
+        private User user;
+        private Principal principal;
+
+        public Authorizer(final User user) {
+            this.user = user;
+            this.principal = new Principal() {
+
+                public String getName() {
+                    return user.username;
+                }
+            };
+        }
+
+        public Principal getUserPrincipal() {
+            return this.principal;
+        }
+
+        public boolean isUserInRole(String role) {
+            return (role.equals(user.role));
+        }
+
+        public boolean isSecure() {
+            return "https".equals(uriInfo.getRequestUri().getScheme());
+        }
+
+        public String getAuthenticationScheme() {
+            return SecurityContext.BASIC_AUTH;
+        }
+    }
+
+    public class User {
+
+        public String username;
+        public String role;
+
+        public User(String username, String role) {
+            this.username = username;
+            this.role = role;
+        }
+    }
+}
---
+++ https-clientserver-grizzly/src/test/java/com/sun/jersey/samples/https_grizzly/MainTest.java
@@ -0,0 +1,126 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ * 
+ * Portions Copyright 2012 Talend
+ */
+
+package com.sun.jersey.samples.https_grizzly;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import java.io.InputStream;
+
+import org.apache.cxf.helpers.IOUtils;
+import org.apache.cxf.jaxrs.client.WebClient;
+import com.sun.jersey.samples.https_grizzly.auth.AuthenticationException;
+
+public class MainTest {
+   
+    private static final String CLIENT_CONFIG_FILE = "ClientConfig.xml";
+    private static final String CLIENT_CONFIG_NOKEY_FILE = "ClientConfigNoKey.xml";
+    
+    @BeforeClass
+    public static void setUp() throws Exception {
+        Server.startServer();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        Server.stopServer();
+    }
+
+    /**
+     * Test to see that the message "CXF HTTPS EXAMPLE" is sent in the response.
+     */
+    @Test
+    public void testSSLWithAuth() {
+//      System.out.println("Sending HTTPS GET to retrieve Hello World string");
+//      System.out.println("Path = " + Server.BASE_URI.toString());
+        WebClient wc = WebClient.create(Server.BASE_URI.toString(), "user", "password",
+           CLIENT_CONFIG_FILE);
+
+        try {
+        	InputStream is = wc.get(InputStream.class);
+            String pageContent = IOUtils.toString(is);
+            System.out.println("Response string returned = " + pageContent);
+            assertEquals(Server.CONTENT, pageContent);
+        } catch (Exception ex) {
+            Assert.fail("Exception reading InputStream: " + ex.getMessage());
+        }
+    }
+
+    /**
+     *
+     * Test to see that HTTP 401 is returned when client tries to GET without
+     * proper basic auth credentials.
+     */
+    @Test
+    public void testHTTPBasicAuth1() {
+        WebClient wc = WebClient.create(Server.BASE_URI.toString(), CLIENT_CONFIG_FILE);
+
+        try {
+        	InputStream is = wc.get(InputStream.class);
+        } catch (Exception ex) {
+        	assertTrue(ex.getMessage().contains("Internal Error"));
+        }
+    }
+
+    /**
+     *
+     * Test to see that SSLHandshakeException is thrown when client doesn't have
+     * trusted key.
+     */
+    @Test
+    public void testSSLAuth1() {
+        WebClient wc = WebClient.create(Server.BASE_URI.toString(), "user", "password",
+       		CLIENT_CONFIG_NOKEY_FILE);
+
+        try {
+        	InputStream is = wc.get(InputStream.class);
+        } catch (Exception ex) {
+            String msg = ex.getMessage();
+            System.out.println("Error message = "  + msg);
+        	assertTrue(msg.contains("ClientWebApplicationException"));
+        }
+    }
+}
